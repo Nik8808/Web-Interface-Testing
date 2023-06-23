81fdc79364b8dcb3b5376b994b2ae5ab327c4584
Nikita <inik8808@gmail.com>
2023-06-23 18:09:15 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/soms7wfjytvsww06?svg=true)](https://ci.appveyor.com/project/Nik8808/web-interface-testing)
---
+++ src/test/java/ru/netology/service/CardOrderTest.java
@@ -0,0 +1,22 @@
+package ru.netology.service;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardOrderTest {
+    @Test
+    void OrdetCard () throws InterruptedException {
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input").setValue("Никита");
+        $("[data-test-id=phone] input").setValue("+12345678901");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $(".paragraph").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+}
